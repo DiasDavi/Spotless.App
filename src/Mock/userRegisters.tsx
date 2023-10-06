c609d89f96dcac1773c21999a6fed1aa5fc90c4d
Davi.Dias <davi.dias@splice.com.br>
2023-10-06 11:46:24 -0300

    feat(userRegisters.tsx): feat mock registers

---
+++ src/Mock/userRegisters.tsx
@@ -0,0 +1,106 @@
+export const usersRegisters = [
+  {
+    name: "Davi",
+    pass: "teste",
+  },
+  {
+    name: "Augusto",
+    pass: "teste2",
+  },
+];
+
+export const ServiceRegisters = [
+  {
+    name: "Davi",
+    password: "12345",
+    serviceType: "Serviços gerais",
+    phone: "(15) 9999-9999",
+    description: "Conserto de máquinas de lavar",
+    address: "Rua Margaridas",
+    price: 50
+  },
+  {
+    name: "Giovanni",
+    password: "54321",
+    serviceType: "Serviços gerais",
+    phone: "(15) 9999-9888",
+    description: "Frete de móveis",
+    address: "Rua Facens",
+    price: 15
+  },
+  {
+    name: "Augusto",
+    password: "12345678",
+    serviceType: "Frete",
+    phone: "(15) 9898-989",
+    description: "Eletricista",
+    address: "Rua Facens",
+    price: 50
+  },
+  {
+    name: "Maria",
+    password: "abcdef",
+    serviceType: "Limpeza de ambientes",
+    phone: "(15) 1111-1111",
+    description: "Limpeza residencial e comercial",
+    address: "Rua das Flores",
+    price: 30
+  },
+  {
+    name: "João",
+    password: "987654",
+    serviceType: "Jardinagem",
+    phone: "(15) 2222-2222",
+    description: "Projeto e manutenção de jardins",
+    address: "Avenida das Árvores",
+    price: 40
+  },
+  {
+    name: "Ana",
+    password: "qwerty",
+    serviceType: "Dedetização",
+    phone: "(15) 3333-3333",
+    description: "Controle de pragas e insetos",
+    address: "Rua dos Insetos",
+    price: 60
+  },
+  {
+    name: "Pedro",
+    password: "zxcvb",
+    serviceType: "Limpeza de piscinas",
+    phone: "(15) 4444-4444",
+    description: "Limpeza e manutenção de piscinas",
+    address: "Avenida da Piscina",
+    price: 70
+  },
+  {
+    name: "Mariana",
+    password: "poiuyt",
+    serviceType: "Montagem de móveis",
+    phone: "(15) 5555-5555",
+    description: "Montagem e desmontagem de móveis",
+    address: "Rua dos Móveis",
+    price: 25
+  },
+  {
+    name: "Lucas",
+    password: "mnbvcx",
+    serviceType: "Pintura",
+    phone: "(15) 6666-6666",
+    description: "Pintura de paredes e superfícies",
+    address: "Avenida das Cores",
+    price: 35
+  }
+];
+
+export const schedule = [
+  {
+    name: "Davi",
+    serviceType: "Serviços gerais",
+    phone: "(15) 9999-9999",
+    description: "Concerto de maquinas de lavar",
+    adress: "Rua Margaridas",
+    price:50,
+    date: "2023-06-10"
+  },
+]
